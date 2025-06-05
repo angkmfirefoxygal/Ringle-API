d282b19ecd2d65f7ed6643131255f23ada32b839
angkmfirefoxygal <angkmfirefoxygal@sookmyung.ac.kr>
2025-06-05 23:26:30 +0900

    feat: 수업 가능 시간 등록 API 구현 (#5)

---
+++ app/controllers/api/v1/slots_controller.rb
@@ -0,0 +1,41 @@
+# app/controllers/api/v1/slots_controller.rb
+class Api::V1::SlotsController < ApplicationController
+    def index
+      start_date = Date.parse(params[:start_date])
+      end_date = Date.parse(params[:end_date])
+      duration = params[:duration].to_i
+  
+      start_times = []
+      (start_date..end_date).each do |date|
+        (0..47).each do |i|  # 30분 단위로 하루 48개
+          time = date.to_time + i * 30.minutes
+          if duration == 30
+            start_times << time if Availability.exists?(start_time: time)
+          elsif duration == 60
+            next_time = time + 30.minutes
+            if Availability.exists?(start_time: time) && Availability.exists?(start_time: next_time)
+              start_times << time
+            end
+          end
+        end
+      end
+  
+      render json: start_times.uniq.map { |t| { start_time: t } }
+    end
+  
+    def available_tutors
+      start_time = Time.parse(params[:start_time])
+      duration = params[:duration].to_i
+  
+      tutors = Tutor.joins(:availabilities)
+      tutors = tutors.where(availabilities: { start_time: start_time })
+  
+      if duration == 60
+        second_block = start_time + 30.minutes
+        tutors = tutors.where(id: Tutor.joins(:availabilities).where(availabilities: { start_time: second_block }).pluck(:id))
+      end
+  
+      render json: tutors.select(:id, :name)
+    end
+  end
+  
