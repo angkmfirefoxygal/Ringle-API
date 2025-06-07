5939284da5135313250861e93060c7ea3a7d78d7
MOON WON <angkmfirefoxygal@sookmyung.ac.kr>
2025-06-07 13:47:06 +0900

    Refactor API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-        resources :availabilities, only: [:create, :destroy]
-      
+      # Primary API routes
+      # POST   /api/v1/availabilities
+      # DELETE /api/v1/availabilities/:id
+      # POST   /api/v1/enrollments
+      # GET    /api/v1/students/:id/enrollments
+      # GET    /api/v1/available_slots
+      # GET    /api/v1/available_tutors
 
-      get '/available_slots', to: 'slots#index'
-      get '/available_tutors', to: 'slots#available_tutors'
+      resources :availabilities, only: %i[create destroy]
+      resources :enrollments, only: %i[create]
 
-      resources :enrollments, only: [:create]
       get '/students/:id/enrollments', to: 'enrollments#index'
+      get '/available_slots',          to: 'slots#index'
+      get '/available_tutors',         to: 'slots#available_tutors'
     end
   end
 end
